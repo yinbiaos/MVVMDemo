e03dad2d7af0c1eedb6a44347ea63739bb0e803c
yinbiaos <156174384@qq.com>
2019-03-20 14:03:09 +0800

    设置沉浸式状态栏

--- app/src/main/java/com/mvvm/demo/BaseActivity.java
+++ app/src/main/java/com/mvvm/demo/BaseActivity.java
@@ -26,6 +26,7 @@ public abstract class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(getContentView());
         mContext = this;
+//        setStatusBar();
         //View注解框架绑定
         unbinder = ButterKnife.bind(this);
     }
@@ -45,5 +46,23 @@ public abstract class BaseActivity extends AppCompatActivity {
             unbinder.unbind();
         }
     }
+
+//    /**
+//     * 设置沉浸式状态栏
+//     */
+//    protected void setStatusBar() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            //5.0及以上
+//            View decorView = getWindow().getDecorView();
+//            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+//            decorView.setSystemUiVisibility(option);
+//            //对状态栏单独设置颜色
+//            getWindow().setStatusBarColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
+//        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            //4.4到5.0
+//            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+//            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+//        }
+//    }
 
 }
