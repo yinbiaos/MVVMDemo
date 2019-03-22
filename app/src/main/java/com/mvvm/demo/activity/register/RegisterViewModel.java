30a7bb31347c0a17f9a8df7a80de4adda1f16535
hnhzy <362070860@qq.com>
2019-03-22 16:01:30 +0800

    项目版块，登录，注册等功能提交

--- app/src/main/java/com/mvvm/demo/activity/register/RegisterViewModel.java
+++ app/src/main/java/com/mvvm/demo/activity/register/RegisterViewModel.java
@@ -12,13 +12,14 @@ import com.mvvm.demo.http.HttpManager;
 import com.mvvm.demo.http.HttpService;
 import com.mvvm.demo.http.RxSchedulers;
 
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.observers.DefaultObserver;
 
 /**
  * Created by hzy on 2019/3/20
  *
  * @author hzy
- * */
+ */
 public class RegisterViewModel extends AndroidViewModel {
 
     private static final String TAG = "ProjectViewModel";
@@ -28,8 +29,11 @@ public class RegisterViewModel extends AndroidViewModel {
      */
     private MutableLiveData<ResponseBean> result = new MutableLiveData<>();
 
+    CompositeDisposable disposable;
+
     public RegisterViewModel(@NonNull Application application) {
         super(application);
+        disposable = new CompositeDisposable();
     }
 
     @Override
@@ -37,6 +41,7 @@ public class RegisterViewModel extends AndroidViewModel {
         super.onCleared();
         //页面销毁时调用
         Logs.d(TAG, "onCleared:");
+        disposable.clear();
     }
 
     public MutableLiveData<ResponseBean> getResult() {
@@ -44,24 +49,13 @@ public class RegisterViewModel extends AndroidViewModel {
     }
 
     public void postRegister(String username, String password, String password2) {
-        HttpManager.getInstance().getService(HttpService.class)
+        disposable.add(HttpManager.getInstance().getService(HttpService.class)
                 .postRegister(username, password, password2)
                 .compose(RxSchedulers.ioMain())
-                .subscribe(new DefaultObserver<ResponseBean>() {
-                    @Override
-                    public void onNext(ResponseBean responseBean) {
-                        result.setValue(responseBean);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Logs.e(TAG, e.toString() + "-----" + e.getMessage());
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
+                .subscribe(responseBean -> {
+                    result.setValue(responseBean);
+                }, throwable -> {
+                    Logs.e(TAG, throwable.toString() + "-----" + throwable.getMessage());
+                }));
     }
 }
