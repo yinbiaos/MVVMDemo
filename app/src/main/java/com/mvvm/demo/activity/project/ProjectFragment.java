dd3974c9fd753d6daff93e8dd804452e186232a3
hnhzy <362070860@qq.com>
2019-03-27 17:27:48 +0800

    修复点赞bug

--- app/src/main/java/com/mvvm/demo/activity/project/ProjectFragment.java
+++ app/src/main/java/com/mvvm/demo/activity/project/ProjectFragment.java
@@ -61,7 +61,8 @@ public class ProjectFragment extends BaseLazyFragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_project, container, false);
     }
 
