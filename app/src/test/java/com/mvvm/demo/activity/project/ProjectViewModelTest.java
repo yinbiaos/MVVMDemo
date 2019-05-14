93e34ac46ea50217fadc13a7ddf78def9d4f3b4a
yinbiaos <156174384@qq.com>
2019-05-14 09:11:37 +0800

    androidx

---
+++ app/src/test/java/com/mvvm/demo/activity/project/ProjectViewModelTest.java
@@ -0,0 +1,46 @@
+package com.mvvm.demo.activity.project;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author yinbiao
+ * @date 2019/4/12
+ */
+public class ProjectViewModelTest {
+
+    @Test
+    public void onCleared() {
+    }
+
+    @Test
+    public void getResult() {
+    }
+
+    @Test
+    public void getProjectList() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCleared1() {
+    }
+
+    @Test
+    public void getResult1() {
+    }
+
+    @Test
+    public void getProjectList1() {
+    }
+}
